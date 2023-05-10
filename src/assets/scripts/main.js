c6de05c2fd4db514ae7aff1c15e84d877cb69425
Marta Perelló <100278106+Martaperello@users.noreply.github.com>
2023-05-10 14:42:29 +0200

    hola

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,14 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+import * as bootstrap from "bootstrap";
+
+( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
+
